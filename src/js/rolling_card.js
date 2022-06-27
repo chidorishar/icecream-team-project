a7ae9c0e93e2ea98b1727100742460fb985f3426
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-06-28 01:16:03 +0300

    Made product cards flipping

--- src/js/rolling_card.js
+++ src/js/rolling_card.js
@@ -1,3 +1,11 @@
-$('.card').click(function () {
-  $(this).toggleClass('is-flipped');
-});
+const productCards = document.querySelectorAll('.product-cards__card');
+
+productCards.forEach(el => el.addEventListener('click', rotateCards));
+
+function rotateCards(event) {
+  if (event.target.nodeName != 'BUTTON') {
+    return;
+  }
+
+  event.currentTarget.classList.toggle('is-flipped');
+}
