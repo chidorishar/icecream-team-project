f72b74fc14f34dc977da9e48ee321670f9a83db2
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-06-29 00:23:31 +0300

    Added rotated avatars

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,9 +1,22 @@
 const domEls = {
   currentButton: document.querySelector('button[data-is-active="true"]'),
   reviewsControls: document.querySelector('.section-reviews__reviews-controls'),
+  avatars: document.querySelectorAll('.section-reviews__avatar'),
+  currentAvatar: document.querySelector(
+    '.section-reviews__avatar:not(.section-reviews__avatar--flipped)'
+  ),
+  avatarsContainer: document.querySelector(
+    '.section-reviews__customers-avatars'
+  ),
 };
 
 domEls.reviewsControls.addEventListener('click', onChangeReview);
+domEls.avatarsContainer.addEventListener('click', () => {
+  const rnd = Math.floor(Math.random() * (3 - 1) + 1);
+
+  flipNthAvatar(rnd);
+  switchToNthReview(rnd);
+});
 
 function onChangeReview(event) {
   const clickTarget = event.target;
@@ -16,9 +29,22 @@ function onChangeReview(event) {
 
   domEls.currentButton.dataset.isActive = 'false';
   domEls.currentButton = clickTarget;
+  const currReviewIndx = domEls.currentButton.dataset.indx;
   domEls.currentButton.dataset.isActive = 'true';
+  switchToNthReview(currReviewIndx);
+  flipNthAvatar(currReviewIndx);
+}
+
+function flipNthAvatar(avtIndx) {
+  console.log(avtIndx);
+  domEls.currentAvatar.classList.add('section-reviews__avatar--flipped');
+  domEls.currentAvatar = domEls.avatars[avtIndx - 1];
+  domEls.currentAvatar.classList.remove('section-reviews__avatar--flipped');
+}
+
+function switchToNthReview(revIndx) {
   document.documentElement.style.setProperty(
     '--current-review-number',
-    domEls.currentButton.dataset.indx
+    revIndx
   );
 }
