4fa915651737d864cdceedebb13e7851adc0752f
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-06-29 19:51:55 +0300

    Renamed script

--- src/js/side-menu.js
+++ src/js/side-menu.js
@@ -1,13 +1,17 @@
 const menuBttns = document.querySelectorAll('[data-menu-button]');
 const mobileMenuRef = document.querySelector('[data-menu]');
+const menuOverlay = document.querySelector('.menu-overlay');
+const menuNav = document.querySelector('.menu__nav-group');
 
 menuBttns.forEach(el => el.addEventListener('click', toggleMenuVisibility));
+menuOverlay.addEventListener('click', toggleMenuVisibility);
+menuNav.addEventListener('click', toggleMenuVisibility);
 
 function toggleMenuVisibility(event) {
   const target = event.currentTarget;
 
   if (target.nodeName != 'BUTTON') {
-    return;
+    // return;
   }
 
   const expanded = target.getAttribute('aria-expanded') === 'true' || false;
