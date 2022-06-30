7c66e96f3db905efb175dfc73a92cf91b328bd56
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-06-30 09:28:09 +0300

    Added handlers for backdrop click and escape press events

--- src/js/locations_window.js
+++ src/js/locations_window.js
@@ -5,14 +5,29 @@
     modal: document.querySelector('[data-location]'),
     scrolling: document.querySelector('[data-location-scrolling'),
   };
+  let isShown = false;
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.modal.addEventListener('click', onBackdropClick);
 
   function toggleModal() {
-    document.body.classList.toggle("location-open");
-    refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle('location-open');
+    refs.modal.classList.toggle('is-hidden');
+    isShown
+      ? document.body.removeEventListener('keydown', onKeyDown)
+      : document.body.addEventListener('keydown', onKeyDown);
+    isShown = !isShown;
   }
 
-})();
+  function onBackdropClick(event) {
+    if (event.target != event.currentTarget) {
+      return;
+    }
+    toggleModal();
+  }
 
+  function onKeyDown(event) {
+    event.code === 'Escape' ? toggleModal() : none;
+  }
+})();
--- src/js/modal__window.js
+++ src/js/modal__window.js
@@ -4,11 +4,29 @@
     closeModalBtn: document.querySelector('[data-close-modal__window]'),
     modal: document.querySelector('[data-modal__window]'),
   };
+  let isShown = false;
 
   refs.openModalBtn.forEach(el => el.addEventListener('click', toggleModal));
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.modal.addEventListener('click', onBackdropClick);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('js-modal-is-hidden');
+    isShown
+      ? document.body.removeEventListener('keydown', onKeyDown)
+      : document.body.addEventListener('keydown', onKeyDown);
+    isShown = !isShown;
+  }
+
+  function onBackdropClick(event) {
+    if (event.target != event.currentTarget) {
+      return;
+    }
+    toggleModal();
+  }
+
+  function onKeyDown(event) {
+    event.code === 'Escape' ? toggleModal() : none;
   }
 })();
